5bb70118a1001252b034ca34d454ee7664f9f156
YeoleSamruddhi <samruddhiyeole02@gmail.com>
2024-12-19 19:16:27 +0530

    Ok

---
+++ frontend/src/App.js
@@ -0,0 +1,63 @@
+// import { Navigate, Route, Routes } from 'react-router-dom';
+// import './App.css';
+// import Login from './pages/Login';
+// import Signup from './pages/Signup';
+// import Home from './pages/Home';
+// import { useState } from 'react';
+// import RefrshHandler from './RefrshHandler';
+
+// function App() {
+//   const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+//   const PrivateRoute = ({ element }) => {
+//     return isAuthenticated ? element : <Navigate to="/login" />
+//   }
+
+//   return (
+//     <div className="App">
+//       <RefrshHandler setIsAuthenticated={setIsAuthenticated} />
+//       <Routes>
+//         <Route path='/' element={<Navigate to="/login" />} />
+//         <Route path='/login' element={<Login />} />
+//         <Route path='/signup' element={<Signup />} />
+//         <Route path='/home' element={<PrivateRoute element={<Home />} />} />
+//       </Routes>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import { Navigate, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Home from './pages/Home';
+import { useState } from 'react';
+import RefrshHandler from './RefrshHandler';
+
+function PrivateRoute({ element, isAuthenticated }) {
+  return isAuthenticated ? element : <Navigate to="/login" />;
+}
+
+function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  return (
+    <div className="App">
+      <RefrshHandler setIsAuthenticated={setIsAuthenticated} />
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/home"
+          element={<PrivateRoute element={<Home />} isAuthenticated={isAuthenticated} />}
+        />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
+
